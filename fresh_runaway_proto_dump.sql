9dd811fddf1ec1162fe740304dc45434879b8be5
= <=>
2016-08-02 23:48:52 -0300

    i updated the db script

--- fresh_runaway_proto_dump.sql
+++ fresh_runaway_proto_dump.sql
@@ -50,7 +50,7 @@ DROP TABLE IF EXISTS `app_parameter`;
 CREATE TABLE `app_parameter` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `key` varchar(255) NOT NULL,
-  `value` text NOT NULL,
+  `value` mediumtext NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -63,6 +63,47 @@ LOCK TABLES `app_parameter` WRITE;
 /*!40000 ALTER TABLE `app_parameter` DISABLE KEYS */;
 INSERT INTO `app_parameter` VALUES (1,'MAX_QUESTIONS','10');
 /*!40000 ALTER TABLE `app_parameter` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fos_user`
+--
+
+DROP TABLE IF EXISTS `fos_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fos_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username_canonical` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_canonical` varchar(255) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `locked` tinyint(1) NOT NULL,
+  `expired` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  `confirmation_token` varchar(255) DEFAULT NULL,
+  `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext NOT NULL COMMENT '(DC2Type:array)',
+  `credentials_expired` tinyint(1) NOT NULL,
+  `credentials_expire_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_957A647992FC23A8` (`username_canonical`),
+  UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=big5;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fos_user`
+--
+
+LOCK TABLES `fos_user` WRITE;
+/*!40000 ALTER TABLE `fos_user` DISABLE KEYS */;
+INSERT INTO `fos_user` VALUES (1,'admin','$2y$13$j9xuyx2bhcg8s00o4s488u5ZtLCrGvrI7Jc2KCxth2iWcqzVow3yC','admin','admin@admin','admin@admin',1,'j9xuyx2bhcg8s00o4s4880wcw000o40','2016-08-02 23:39:07',0,0,NULL,NULL,NULL,'a:1:{i:0;s:16:\"ROLE_SUPER_ADMIN\";}',0,NULL);
+/*!40000 ALTER TABLE `fos_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -100,8 +141,10 @@ DROP TABLE IF EXISTS `participant`;
 CREATE TABLE `participant` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `gender` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=211 DEFAULT CHARSET=big5;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,26 +165,27 @@ DROP TABLE IF EXISTS `participant_response`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `participant_response` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `session_id` int(11) NOT NULL,
-  `first_image_served_id` int(11) NOT NULL,
-  `second_image_served_id` int(11) NOT NULL,
-  `third_image_served_id` int(11) NOT NULL,
-  `correct_image_id` int(11) NOT NULL,
-  `selected_image_id` int(11) NOT NULL,
+  `session_id` int(11) DEFAULT NULL,
+  `first_image_served_id` int(11) DEFAULT NULL,
+  `second_image_served_id` int(11) DEFAULT NULL,
+  `third_image_served_id` int(11) DEFAULT NULL,
+  `correct_image_id` int(11) DEFAULT NULL,
+  `selected_image_id` int(11) DEFAULT NULL,
+  `pointsEarned` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_participant_response_image1_idx` (`first_image_served_id`),
   KEY `fk_participant_response_image2_idx` (`second_image_served_id`),
   KEY `fk_participant_response_image3_idx` (`third_image_served_id`),
   KEY `fk_participant_response_image4_idx` (`correct_image_id`),
   KEY `fk_participant_response_image5_idx` (`selected_image_id`),
-  KEY `fk_participant_response_session_idx` (`session_id`),
+  KEY `fk_participant_response_session_participant_idx` (`session_id`),
+  CONSTRAINT `fk_participant_response_session` FOREIGN KEY (`session_id`) REFERENCES `participant_session` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_participant_response_image1` FOREIGN KEY (`first_image_served_id`) REFERENCES `image` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_participant_response_image2` FOREIGN KEY (`second_image_served_id`) REFERENCES `image` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_participant_response_image3` FOREIGN KEY (`third_image_served_id`) REFERENCES `image` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_participant_response_image4` FOREIGN KEY (`correct_image_id`) REFERENCES `image` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_participant_response_image5` FOREIGN KEY (`selected_image_id`) REFERENCES `image` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_participant_response_session` FOREIGN KEY (`session_id`) REFERENCES `participant_session` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=big5;
+  CONSTRAINT `fk_participant_response_image5` FOREIGN KEY (`selected_image_id`) REFERENCES `image` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,7 +206,7 @@ DROP TABLE IF EXISTS `participant_session`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `participant_session` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `participant_id` int(11) NOT NULL,
+  `participant_id` int(11) DEFAULT NULL,
   `http_session` varchar(1024) NOT NULL,
   `started_at` datetime NOT NULL,
   `ended_at` datetime DEFAULT NULL,
@@ -170,7 +214,7 @@ CREATE TABLE `participant_session` (
   PRIMARY KEY (`id`),
   KEY `fk_session_participant_participant_idx` (`participant_id`),
   CONSTRAINT `fk_participant_session_participant` FOREIGN KEY (`participant_id`) REFERENCES `participant` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=211 DEFAULT CHARSET=big5;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -193,9 +237,9 @@ CREATE TABLE `training_task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `training_step` int(11) NOT NULL,
   `help_text` text,
-  `first_image` int(11) NOT NULL,
-  `second_image` int(11) NOT NULL,
-  `third_image` int(11) NOT NULL,
+  `first_image` int(11) DEFAULT NULL,
+  `second_image` int(11) DEFAULT NULL,
+  `third_image` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_training_task_image1_idx` (`first_image`),
   KEY `fk_training_task_image2_idx` (`second_image`),
@@ -224,4 +268,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-28  1:29:44
+-- Dump completed on 2016-08-02 23:48:05
